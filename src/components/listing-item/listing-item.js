6d3a44e08eb3080b17fdaff24da1ad8c1dc318e4
Rafał Rudol <rrrrudol@gmail.com>
2018-10-28 17:40:42 +0100

    fix

--- src/components/listing-item/listing-item.js
+++ src/components/listing-item/listing-item.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import moment from "moment";
 import "moment/locale/en-gb";
 
@@ -6,41 +6,42 @@ class ListingItem extends React.Component {
   render() {
     const event = this.props.event;
 
-    const customStyles = !this.props.disabled ? {} : { color: '#888' };
+    const customStyles = !this.props.disabled ? {} : { color: "#888" };
 
-    return <div className="hackathon-entry">
-      <article style={customStyles}>
-        <div class="tags">
-          <span class="city">@{event.location}</span>
-          {event.tags.map(tag => (
+    return (
+      <div className="hackathon-entry">
+        <article style={customStyles}>
+          <div class="tags">
+            <span class="city">@{event.location}</span>
+            {event.tags.map(tag => (
+              <span>#{tag}</span>
+            ))}
+          </div>
+          <h3>{event.title}</h3>
+          <div class="metadata">
             <span>
-              #{tag}
+              {moment(event.date).format("LL")} -
+              {" " + moment(event.date).format("dddd")}
+              {this.props.disabled
+                ? ""
+                : ` - ${moment(event.date)
+                    .endOf("hours")
+                    .diff(new Date(), "days")} day(s) left`}
             </span>
-          ))}
-        </div>
-        <h3>{event.title}</h3>
-        <div class="metadata">
-          <span>
-            {moment(event.date).format("LL")}
-
-            {this.props.disabled ? '' : ` - ${moment(event.date)
-              .endOf("hours")
-              .diff(new Date(), "days")} day(s) left`}
-          </span>
-
-        </div>
-        <p>
-          {event.description}
-        </p>
-      </article>
-      <footer>
-        {this.props.disabled ? '' : (
-          <a href={event.url}>
-            website
-          </a>
-        )}
-      </footer>
-    </div>
+          </div>
+          <p>{event.description}</p>
+        </article>
+        <footer>
+          {this.props.disabled ? (
+            ""
+          ) : (
+            <a target="_blank" href={event.url}>
+              website
+            </a>
+          )}
+        </footer>
+      </div>
+    );
   }
 }
 
