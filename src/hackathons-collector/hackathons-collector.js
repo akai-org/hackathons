80c4d1ca81324b04e2ecc3eb5c172765aa172c19
Damian Kalka <kalka762@gmail.com>
2020-09-24 22:18:26 +0200

    migrated HackathonsCollector from class to functional component

--- src/hackathons-collector/hackathons-collector.js
+++ src/hackathons-collector/hackathons-collector.js
@@ -16,13 +16,12 @@ import Filter from './styled-components/filter';
 import Listing from './logic-components/listing';
 
 // Data
-import events from '../events';
 
 // Assets
 import logo from './assets/logo.svg';
 import akai from './assets/akai-logo.svg';
 
-const HackathonsCollector = () => {
+const HackathonsCollector = ({ events }) => {
   const [filterWeekends, changeFilterWeekends] = useState(false);
   const [allEvents] = useState(events);
 
@@ -35,7 +34,7 @@ const HackathonsCollector = () => {
     .reverse()
     .splice(0, 3);
 
-  const events = allEvents
+  const updatedEvents = allEvents
     .filter(({ date }) => date > new Date())
     .filter(({ date }) => {
       return !filterWeekends || date.getDay() > 5;
@@ -59,7 +58,7 @@ const HackathonsCollector = () => {
       <Main>
         <Section>
           <H2>Incoming hackathons:</H2>
-          <Listing list={events} />
+          <Listing list={updatedEvents} />
         </Section>
         <Section>
           <H2>Previous hackathons:</H2>
@@ -77,61 +76,3 @@ const HackathonsCollector = () => {
 };
 
 export default HackathonsCollector;
-
-// export default class HackathonsCollector extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       filterWeekends: false,
-//       allEvents: events,
-//     };
-//   }
-//   render() {
-//     const previousEvents = this.state.allEvents
-//       .filter((event) => event.date <= new Date())
-//       .filter((event) => {
-//         return !this.state.filterWeekends || event.date.getDay() > 5;
-//       })
-//       .sort((a, b) => a.date - b.date)
-//       .reverse()
-//       .splice(0, 3);
-//     const events = this.state.allEvents
-//       .filter((event) => event.date > new Date())
-//       .filter((event) => {
-//         return !this.state.filterWeekends || event.date.getDay() > 5;
-//       })
-//       .sort((a, b) => a.date - b.date);
-//     return (
-//       <div>
-//         <Header>
-//           <Logo src={logo} />
-//           <H1>Hackathons Collector</H1>
-//         </Header>
-//         <Filter
-//           value={this.state.filterWeekends}
-//           setValue={(newValue) => {
-//             this.setState({ filterWeekends: newValue });
-//           }}
-//         >
-//           Only weekend events
-//         </Filter>
-//         <Main>
-//           <Section>
-//             <H2>Incoming hackathons:</H2>
-//             <Listing list={events} />
-//           </Section>
-//           <Section>
-//             <H2>Previous hackathons:</H2>
-//             <Listing list={previousEvents} disabled={true} />
-//           </Section>
-//         </Main>
-//         <Footer>
-//           Created by{' '}
-//           <a href='https://akai.org.pl/' title='AKAI'>
-//             <img src={akai} alt='AKAI' height='12' />
-//           </a>
-//         </Footer>
-//       </div>
-//     );
-//   }
-// }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HackathonsCollector } from './hackathons-collector/';
 import registerServiceWorker from './registerServiceWorker';
+import events from './events';
 
-ReactDOM.render(<HackathonsCollector />, document.getElementById('root'));
+ReactDOM.render(<HackathonsCollector events={events} />, document.getElementById('root'));
 registerServiceWorker();
