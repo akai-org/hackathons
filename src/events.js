7d63014e65ac49e22a91b5158c0d448f2e40476f
PatrykGrzegorek <51320419+PatrykGrzegorek@users.noreply.github.com>
2020-10-14 22:28:42 +0200

    Update events.js

--- src/events.js
+++ src/events.js
@@ -8,6 +8,13 @@ export default [
     tags: [],
   },
   {
+    title: 'Spacehack',
+    date: new Date(2020, 10, 23),
+    url: 'http://spacehack.pl/#home',
+    location: 'Online',
+    tags: [],
+  },   
+  {
     title: 'Hacktoberfest',
     date: new Date(2020, 10, 1),
     url: 'https://hacktoberfest.digitalocean.com/',
